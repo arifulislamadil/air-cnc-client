3f67e2a9959be1c3d54fca75427ba492620643af
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-08-30 17:02:05 +0400

    get data from Db

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,25 +1,35 @@
 // save a user to database
-export const saveUser = user=>{
-    const currentUser = {
-        email: user.email,
-    }
+export const saveUser = (user) => {
+  const currentUser = {
+    email: user.email,
+  };
 
-    fetch(`${import.meta.env.VITE_API_URL}/users/${user?.email}`,{
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json'},
-    body: JSON.stringify(currentUser)
-}).then(res=>res.json()).then(data=>console.log(data))
-}
+  fetch(`${import.meta.env.VITE_API_URL}/users/${user?.email}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(currentUser),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
 
 // become host
-export const becomeHost = email=>{
-    const currentUser = {
-        role: "host",
-    }
+export const becomeHost = (email) => {
+  const currentUser = {
+    role: "host",
+  };
 
-    fetch(`${import.meta.env.VITE_API_URL}/users/${email}`,{
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json'},
-    body: JSON.stringify(currentUser)
-}).then(res=>res.json()).then(data=>console.log(data))
-}
+  return fetch(`${import.meta.env.VITE_API_URL}/users/${email}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(currentUser),
+  }).then((res) => res.json());
+};
+
+// Get role from Database
+export const getRole = async (email) => {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/users/${email}`)
+  const user = await response.json();
+  return user?.role;
+};
--- src/components/Shared/NavBar/MenuDropdown.jsx
+++ src/components/Shared/NavBar/MenuDropdown.jsx
@@ -1,73 +1,96 @@
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu } from "react-icons/ai";
 // import Avatar from './Avatar'
-import { useCallback, useContext, useState } from 'react'
-import { AuthContext } from '../../../providers/AuthProvider'
-import { Link } from 'react-router-dom'
-import Avatar from './Avatar'
+import { useCallback, useContext, useState } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { Link } from "react-router-dom";
+import Avatar from "./Avatar";
+import HostRequestModal from "../../Modal/HostRequestModal";
+import { becomeHost } from "../../../api/auth";
+import { toast } from "react-hot-toast";
 
 const MenuDropdown = () => {
-  const { user, logOut } = useContext(AuthContext)
-  const [isOpen, setIsOpen] = useState(false)
+  const { user, logOut } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [model, setModal] = useState(false);
+
+  const modalHandler = (email) => {
+   becomeHost(email).then(data=>{
+    console.log(data)
+    toast.success("you are host now,Post Rooms!")
+    closeModal()
+   })
+  };
+
+
+const closeModal = () => {
+  setModal(false);
+}
+
+
+
+
+
+
+
+
   const toggleOpen = useCallback(() => {
-    setIsOpen(value => !value)
-  }, [])
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
-    <div className='relative'>
-      <div className='flex flex-row items-center gap-3'>
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
         {/* Aircnc btn  */}
-        <div className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
+        <div onClick={()=>setModal(true)} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
           AirCNC your home
         </div>
         {/* Dropdown btn  */}
         <div
           onClick={toggleOpen}
-          className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
-          <div className='hidden md:block'>
-           <Avatar/>
+          <div className="hidden md:block">
+            <Avatar />
           </div>
         </div>
       </div>
       {isOpen && (
-        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
-          <div className='flex flex-col cursor-pointer'>
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
             <Link
-              to='/'
-              className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+              to="/"
+              className="block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold"
             >
               Home
             </Link>
             {user ? (
               <>
-               <Link
-                  to='/dashboard'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                <Link
+                  to="/dashboard"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Dashboard
                 </Link>
 
                 <div
-                onClick={logOut}
-                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
-              >
-                Logout
-              </div>
-              
-              
-              
+                  onClick={logOut}
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer"
+                >
+                  Logout
+                </div>
               </>
             ) : (
               <>
                 <Link
-                  to='/login'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                  to="/login"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Login
                 </Link>
                 <Link
-                  to='/signup'
-                  className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                  to="/signup"
+                  className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
                 >
                   Sign Up
                 </Link>
@@ -76,8 +99,9 @@ const MenuDropdown = () => {
           </div>
         </div>
       )}
+      <HostRequestModal closeModal={closeModal} email={user?.email} isOpen={model} modalHandler={modalHandler} />
     </div>
-  )
-}
+  );
+};
 
-export default MenuDropdown
+export default MenuDropdown;
